1ef19ecec408408ff29b28eb02a06242a9fee6b9
YOUR NAME <61445797+khaled1113@users.noreply.github.com>
2026-03-27 07:58:08 +0200

    27/3/2025

--- 7_Segment_Decoder.v
+++ 7_Segment_Decoder.v
@@ -1,19 +1,19 @@
-module 7_Segment_Decoder (A,B,C,D,led_a,led_b,led_c,led_d,led_e,led_f,led_g);
+module Seven_Segment_Decoder (A,B,C,D,led_a,led_b,led_c,led_d,led_e,led_f,led_g);
 input A,B,C,D;
-output led_a,led_b,led_c,led_d,led_e,led_f,led_g;
+output wire led_a, led_b, led_c, led_d, led_e, led_f, led_g;
 
-assign led_a = A | C | (B & D) | (~B & ~D);
+assign led_a = (A) | (C) | ((B) & (D)) | ((~B) & (~D));
 
-assign led_b = ~B | (~C & ~D) | (C & D);
+assign led_b = (~B) | ((~C) & (~D)) | (C & D);
 
-assign led_c = B | ~C | D;
+assign led_c = (B) | (~C) | (D);
 
-assign led_d = A | (~B & C) | (C & ~D) | (~B & ~D) | (B & D & ~C);
+assign led_d = (A) | ((~B) & (C)) | ((C) & (~D)) | ((~B) & (~D)) | ((B) & (D) & (~C));
 
-assign led_e = (~D) & (~B | C);
+assign led_e = ((~D)) & ((~B) | (C));
 
-assign led_f = A | (~C & ~D) | (B & (~C | ~D));
+assign led_f = (A) | ((~C) & (~D)) | ((B) & ((~C) | (~D)));
 
-assign led_g = A | (B & ~C) | (~B & C) | (C & ~D);
+assign led_g = (A) | ((B) & (~C)) | ((~B) & (C)) | ((C) & (~D));
 
 endmodule
--- 7_Segment_Decoder_tb.v
+++ 7_Segment_Decoder_tb.v
@@ -1,4 +1,35 @@
-module 7_segment_Decoder_tb;
-reg  led_a,led_b,led_c,led_d,led_e,led_f,led_g;
+module SevenSegmentDecoder_tb;
+reg a, b, c, d;
 
-7_Segment_Decoder 7_Segment_Decoder_dut()
+7_Segment_Decoder dut(
+    .A(a),
+    .B(b),
+    .C(c),
+    .D(d)
+);
+
+initial begin 
+    #10 a= 0; b=0; c=0; d=0;
+    #10 a= 0; b=0; c=0; d=1;
+    #10 a= 0; b=0; c=1; d=0;
+    #10 a= 0; b=0; c=1; d=1;   
+    #10 a= 0; b=1; c=0; d=0;
+    #10 a= 0; b=1; c=0; d=1;
+    #10 a= 0; b=1; c=1; d=0;
+    #10 a= 0; b=1; c=1; d=1;
+    #10 a= 1; b=0; c=0; d=0;
+    #10 a= 1; b=0; c=0; d=1;
+    #10 a= 1; b=0; c=1; d=0;
+    #10 a= 1; b=0; c=1; d=1;
+    #10 a= 1; b=1; c=0; d=0;
+    #10 a= 1; b=1; c=0; d=1;
+    #10 a= 1; b=1; c=1; d=0;
+    #10 a= 1; b=1; c=1; d=1;
+    #10 $finish;
+end
+//display and monitor
+initial begin
+    $display ("Time   A   B   C   D   led_a  led_b  led_c  led_d  led_e  led_f  led_g");
+    $monitor ("%d %b %b %b %b %b %b %b %b %b %b %b", $time, a, b, c, d, dut.led_a, dut.led_b, dut.led_c, dut.led_d, dut.led_e, dut.led_f, dut.led_g);
+end
+endmodule
